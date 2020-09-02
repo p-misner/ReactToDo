d90a14e7be7c2ea69b53ebf3698a54e56ea9bdad
Priya Misner <priya.misner@tufts.edu>
2020-09-02 16:20:31 -0700

    Updates

---
+++ precache-manifest.4d2cd594e1de3ba6e802fad5a1a42177.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "266ef43a6d5fdaaa5bd0a499de625115",
+    "url": "/ReactToDo/index.html"
+  },
+  {
+    "revision": "4ac605219e7985662357",
+    "url": "/ReactToDo/static/css/main.b94dad1f.chunk.css"
+  },
+  {
+    "revision": "ffa0ce1f5bcb805acb5f",
+    "url": "/ReactToDo/static/js/2.cbb27b25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactToDo/static/js/2.cbb27b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ac605219e7985662357",
+    "url": "/ReactToDo/static/js/main.7242a413.chunk.js"
+  },
+  {
+    "revision": "cb02eb8b21ca8fab5752",
+    "url": "/ReactToDo/static/js/runtime-main.b71d775c.js"
+  }
+]);
